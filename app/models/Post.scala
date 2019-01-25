34a2a02497c5114effd0e6a41ef359d824d5751f
konstantinmusienko <konstantin.musienko@sysgears.com>
2019-01-25 19:31:58 +0200

    Add 'authodId' field to the Post entity

--- app/models/Post.scala
+++ app/models/Post.scala
@@ -9,4 +9,5 @@ package models
   */
 case class Post(id: Option[Long] = None,
                 title: String,
-                content: String)
+                content: String,
+                authorId: Long)
