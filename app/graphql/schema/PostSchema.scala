169db9097e6da8cfb5542452435a9a58f857a8bc
konstantinmusienko <konstantin.musienko@sysgears.com>
2019-01-25 19:37:22 +0200

    Update PostSchema to add an ability to fetch an author of the post

--- app/graphql/schema/PostSchema.scala
+++ app/graphql/schema/PostSchema.scala
@@ -2,20 +2,57 @@ package graphql.schema
 
 import com.google.inject.Inject
 import graphql.resolvers.PostResolver
-import models.Post
-import sangria.macros.derive.{ObjectTypeName, deriveObjectType}
+import models.errors.NotFound
+import models.{Post, User}
+import repositories.{PostRepository, PostRepositoryImpl, UserRepository, UserRepositoryImpl}
+import sangria.macros.derive.{AddFields, ObjectTypeName, ReplaceField, deriveObjectType}
 import sangria.schema._
+
+import scala.concurrent.{ExecutionContext, Future}
 
 /**
   * Defines GraphQL schema for the Post entity.
   */
-class PostSchema @Inject()(postResolver: PostResolver) {
+class PostSchema @Inject()(postResolver: PostResolver,
+                           userRepository: UserRepository,
+                           postRepository: PostRepository,
+                           implicit val executionContext: ExecutionContext) {
 
   /**
     * Sangria's representation of the Post type.
     * It's necessary to convert Post object into Sangria's GraphQL object to represent it in the GraphQL format.
     */
-  implicit val PostType: ObjectType[Unit, Post] = deriveObjectType[Unit, Post](ObjectTypeName("Post"))
+  implicit val PostType: ObjectType[Unit, Post] = deriveObjectType[Unit, Post](ObjectTypeName("Post"),
+    ReplaceField(
+      fieldName = "authorId",
+      field =
+        Field(
+          name = "author",
+          fieldType = AuthorType,
+          resolve = ctx => {
+            val authorId = ctx.value.authorId
+            userRepository.find(authorId).flatMap {
+              case Some(author) => Future.successful(author)
+              case _ => Future.failed(NotFound(s"User with id = $authorId"))
+            }
+          }
+        )
+    )
+  )
+
+  implicit val AuthorType: ObjectType[Unit, User] = deriveObjectType[Unit, User](ObjectTypeName("Author"),
+    AddFields(
+      Field(
+        name = "posts",
+        fieldType = ListType(PostType),
+        resolve = ctx => {
+          //TODO: get rid of 'get' on Option
+          val authorId = ctx.value.id.get
+          postRepository.findByAuthorId(authorId)
+        }
+      )
+    )
+  )
 
   /**
     * List of GraphQL queries defined for the Post type.
@@ -47,12 +84,14 @@ class PostSchema @Inject()(postResolver: PostResolver) {
       fieldType = PostType,
       arguments = List(
         Argument("title", StringType),
-        Argument("content", StringType)
+        Argument("content", StringType),
+        Argument("authorId", LongType)
       ),
       resolve = sangriaContext =>
         postResolver.addPost(
           sangriaContext.args.arg[String]("title"),
-          sangriaContext.args.arg[String]("content")
+          sangriaContext.args.arg[String]("content"),
+          sangriaContext.args.arg[Long]("authorId")
         )
     ),
     Field(
@@ -61,13 +100,15 @@ class PostSchema @Inject()(postResolver: PostResolver) {
       arguments = List(
         Argument("id", LongType),
         Argument("title", StringType),
-        Argument("content", StringType)
+        Argument("content", StringType),
+        Argument("authorId", LongType)
       ),
       resolve = sangriaContext =>
         postResolver.updatePost(
           sangriaContext.args.arg[Long]("id"),
           sangriaContext.args.arg[String]("title"),
-          sangriaContext.args.arg[String]("content")
+          sangriaContext.args.arg[String]("content"),
+          sangriaContext.args.arg[Long]("authorId")
         )
     ),
     Field(
